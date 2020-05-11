bf275f32d78ba29840208539e99a08928474bafa
kruppatomi <hungaryca@gmail.com>
2020-05-11 11:20:07 +0200

    methods for create and insert keywords was added

---
+++ ReactASPCrud/ReactASPCrud/Models/Table.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactASPCrud.Models
+{
+    public class Table
+    {
+        public string Name { get; set; }
+
+        public List<string> serialisedRecords = new List<string>();
+        public List<ExpandoObject> deserialisedRecords = new List<ExpandoObject>();
+        public Dictionary<string, string> columns = new Dictionary<string, string>();
+
+        public void addRecord(string jsonstring, ExpandoObject expando)
+        {
+            serialisedRecords.Add(jsonstring);
+            deserialisedRecords.Add(expando);
+        }
+
+        public void addHeader(string propName, string value)
+        {
+            columns.Add(propName, value);
+        }
+    }
+}
