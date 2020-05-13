45cd2ff616f5b1820080f455594266c80f7cc9b5
kruppatomi <hungaryca@gmail.com>
2020-05-13 15:24:51 +0200

    flexible frontend tables

--- react-ui-crud/src/components/DataTable.js
+++ react-ui-crud/src/components/DataTable.js
@@ -4,58 +4,126 @@ import { API_URL } from '../constants';
 
 class DataTable extends Component {
 
-  deleteItem = id => {
-    let confirmDeletion = window.confirm('Do you really wish to delete it?');
-    if (confirmDeletion) {
-      fetch(`${API_URL}/${id}`, {
-        method: 'delete',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        .then(res => {
-          this.props.deleteItemFromState(id);
-        })
-        .catch(err => console.log(err));
-    }
-  }
-
   render() {
     const items = this.props.items;
-    return <Table striped>
-      <thead className="thead-dark">
-        <tr>
-          <th>AGE</th>
-          <th>Name</th>
-          <th>NICKNAME</th>
-          <th>SEX</th>
-        </tr>
-      </thead>
-      <tbody>
-        {!items || items.length <= 0 ?
-          <tr>
-            <td colSpan="6" align="center"><b>No Users yet</b></td>
-          </tr>
-          : items.map(item => (
+
+    return <div>
+    {items.map(item => (
+      <div>
+      <h1>{item.name}</h1>
+      <Table striped>
+        <thead className="thead-dark">
+          {item.deserialisedRecords.slice(0,1).map(srec => (
             <tr>
-        
-              <td>
-                {item.age}
-              </td>
-              <td>
-                {item.name}
-              </td>
-              <td>
-                {item.nickname}
-              </td>
-              <td>
-                {item.sex}
-              </td>
+            {Object.keys(srec).map(function(key)
+              { return <th>{key}</th>;})} 
+            </tr>
+            ))}
+        </thead>
+        <tbody>
+          {item.deserialisedRecords.map(srec => (
+            <tr>
+              {Object.keys(srec).map(function(key)
+                { return <td>{srec[key]}</td>;})} 
             </tr>
           ))}
-      </tbody>
-    </Table>;
+        </tbody> 
+     </Table>
+     </div>
+    ))}
+    </div>
   }
 }
 
 export default DataTable;
+
+
+// render() {
+//   const items = this.props.items;
+//   return (
+//     <div>
+//       <h1>hello</h1>
+//       <tr>
+//         {
+//           items.map(item => (
+//             <p>
+//               {item.deserialisedRecords} 
+//             {/* {Object.keys(item.deserialisedRecords[0]).map(function(key)
+//             { return <div>
+//               Key: {key}, Value: {item.deserialisedRecords[0][key]}
+//               </div>;
+//              })} */}
+//             </p>
+//               ))
+         
+//         }
+//       </tr>
+//       </div>
+//   )
+// }
+  // deleteItem = id => {
+  //   let confirmDeletion = window.confirm('Do you really wish to delete it?');
+  //   if (confirmDeletion) {
+  //     fetch(`${API_URL}/${id}`, {
+  //       method: 'delete',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     })
+  //       .then(res => {
+  //         this.props.deleteItemFromState(id);
+  //       })
+  //       .catch(err => console.log(err));
+  //   }
+  // }
+
+
+// EZEGESZENJOVOLT
+  // render() {
+  //   const items = this.props.items;
+  //   return <div>
+  // <h2>valami {items.length}</h2>
+  //   {items.map(item => (
+  //    <p> {item.name}</p>
+  //   ))}
+  //   </div>
+  // }
+//     const items = this.props.items;
+//     return <Table striped>
+//       {/* <thead className="thead-dark">
+//         <tr>
+//           <th>AGE</th>
+//           <th>Name</th>
+//           <th>NICKNAME</th>
+//           <th>SEX</th>
+//         </tr>
+//       </thead> */}
+//       <tbody>
+//         {!items || items.length <= 0 ?
+//           <tr>
+//             <td colSpan="6" align="center"><b>No Users yet</b></td>
+//           </tr>
+//           : items.map(function(item, index){
+//             return index
+//           })
+//           // : items.map(item => (
+//           //   <tr>
+        
+//           //     <td>
+//           //       {item.age}
+//           //     </td>
+//           //     <td>
+//           //       {item.name}
+//           //     </td>
+//           //     <td>
+//           //       {item.nickname}
+//           //     </td>
+//           //     <td>
+//           //       {item.sex}
+//           //     </td>
+//           //   </tr>
+//           }
+//       </tbody>
+//     </Table>;
+//   }
+// }
