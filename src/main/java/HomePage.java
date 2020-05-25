912a8a3db603e5cb7374e348adf26877254b6fd4
rebak <rebakbalazs@gmail.com>
2020-05-25 09:28:44 +0200

    select, delete tests

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage extends BasePage {
@@ -47,6 +48,7 @@ public class HomePage extends BasePage {
     }
 
     public boolean checkTableTitle() {
+        wait.until(ExpectedConditions.visibilityOf(checkTableTitle));
         return checkTableTitle.isDisplayed();
     }
 
